57d3ae91c8bd54d81ec1bad8a7168c7ffde8377a
Viktor Pogrebniak <avtomaton@gmail.com>
2015-04-22 12:41:09 +0300

    code format, exec flag

--- gen/sample/background.py
+++ gen/sample/background.py
@@ -1,8 +1,5 @@
-#
-#
-#
+#!/usr/bin/python
 
-#
 import sys
 import os
 import numpy
@@ -10,52 +7,40 @@ from PIL import Image
 import struct
 import argparse
 
-#
 parser = argparse.ArgumentParser()
 parser.add_argument('src')
 args = parser.parse_args()
 
-#
 src = args.src
 
-#
+
 def write_rid(im):
-	#
-	# raw intensity data
-	#
+    # raw intensity data
+    h = im.shape[0]
+    w = im.shape[1]
 
-	#
-	h = im.shape[0]
-	w = im.shape[1]
+    hw = struct.pack('ii', h, w)
 
-	#
-	hw = struct.pack('ii', h, w)
+    tmp = [None] * w * h
+    for y in range(0, h):
+        for x in range(0, w):
+            tmp[y * w + x] = im[y, x]
 
-	tmp = [None]*w*h
-	for y in range(0, h):
-		for x in range(0, w):
-			tmp[y*w + x] = im[y, x]
+    pixels = struct.pack('%sB' % w * h, *tmp)
 
-	#
-	pixels = struct.pack('%sB' % w*h, *tmp)
+    sys.stdout.buffer.write(hw)
+    sys.stdout.buffer.write(pixels)
 
-	#
-	sys.stdout.buffer.write(hw)
-	sys.stdout.buffer.write(pixels)
-
-#
 for dirpath, dirnames, filenames in os.walk(src):
-	for filename in filenames:
-		#
-		path = dirpath + '/' + filename
+    for filename in filenames:
+        #
+        path = dirpath + '/' + filename
 
-		#
-		try:
-			im = numpy.asarray(Image.open(path).convert('L'))
-		except:
-			continue
+        try:
+            im = numpy.asarray(Image.open(path).convert('L'))
+        except RuntimeError:
+            continue
 
-		#
-		write_rid(im)
-		sys.stdout.buffer.write( struct.pack('i', 0) )
+        write_rid(im)
+        sys.stdout.buffer.write(struct.pack('i', 0))
 
